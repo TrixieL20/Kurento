115660d6aa7e7bb7c4ed0a3a614c108cd5497c9a
minjung <minjung0768@gmail.com>
2025-01-24 19:00:07 +0900

    setting

---
+++ src/test/java/org/improvejava/kurento_chat/KurentoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.improvejava.kurento_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurentoChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
